c380e3f65311805d65c8e5f6d24ce057494e4072
Adam <madacgrav@gmail.com>
2024-02-01 09:54:27 -0600

    adding script formatting to formalize script

--- Import-ADOUsers.ps1
+++ Import-ADOUsers.ps1
@@ -1,9 +1,60 @@
+#
+<#
+.SYNOPSIS
+  This script will import users from json export file from Azure DevOps Server to Azure DevOps Services.
+.DESCRIPTION
+  This script will import users from json export file from Azure DevOps Server to Azure DevOps Services.
+.PARAMETER domainName
+    The name of the Active Directory domain in the <domainName>\<username> format.
+.PARAMETER ADOorgName
+    The name of the Azure DevOps Services organization.
+.PARAMETER ADOPAT
+    The Personal Access Token for the Azure DevOps Services organization.
+.PARAMETER jsonPathandFileName
+    The path and file name of the json file exported from Azure DevOps Server.
+.PARAMETER identityMapPath
+    The path and file name of the csv file that maps the Active Directory users to their email addresses.
+.INPUTS
+  None
+.OUTPUTS
+  None
+.NOTES
+  Author:         Adam Graves
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+  Import-ADOUsers.ps1 -domainName "contoso" -ADOorgName "contoso" -ADOPAT "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" -jsonPathandFileName "C:\Users\adamg\Documents\ADSExport.json" -identityMapPath "C:\Users\adamg\Documents\identityMap.csv"
+#>
 [cmdletBinding()]
 param (
     [string]$domainName,
     [string]$ADOorgName,
-    [string]$ADOpat
+    [string]$ADOpat,
+    [string]$jsonPathandFileName,
+    [string]$identityMapPath
 )
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+#Set Error Action to Silently Continue
+$ErrorActionPreference = "SilentlyContinue"
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+$identityMap = Import-Csv -Path $identityMapPath
+$ADSGroupInfo = Get-Content -Path $jsonPathandFileName | ConvertFrom-Json
+$ADOGroupInfo = Get-ADOGroupInfo -PAT $ADOpat -orgName $ADOorgName
+$ADOUserInfo = Get-ADOUserInfo -PAT $ADOpat -orgName $ADOorgName
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+
+Function LogWrite([string]$logstring)
+{
+    $Logfile = ".\Logging\log.txt"
+    $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+    Add-content $Logfile -value "$Stamp - $logstring"
+}
 
 function Get-ADOGroupInfo ([string]$PAT, [string]$orgName) {
     $username = ""
@@ -29,6 +80,29 @@ function Get-ADOGroupInfo ([string]$PAT, [string]$orgName) {
     return $adoGroups
 }
 
+function Get-ADOGroupInfo ([string]$PAT, [string]$orgName) {
+    $username = ""
+    $Auth = '{0}:{1}' -f $username, $PAT
+    $Auth = [System.Text.Encoding]::UTF8.GetBytes($Auth)
+    $Auth = [System.Convert]::ToBase64String($Auth)
+    $header = @{Authorization = ("Basic {0}" -f $Auth) }
+
+    $groupApiUrl = "https://vssps.dev.azure.com/$orgName/_apis/graph/groups?api-version=7.1-preview.1"
+    $groups = (Invoke-RestMethod -Uri $groupApiUrl -Headers $header -Method GET  -ContentType application/json).value
+
+    $adoGroups = @()
+
+    foreach ($group in $groups) {
+        if ($group.origin -eq "vsts") {
+            $groupInfo = [psobject]@{
+                Name = $group.principalName
+                id = $group.descriptor
+            }
+            $adoGroups += $groupInfo
+        }
+    }
+    return $adoGroups
+}
 
 function Get-ADOUserInfo ([string]$PAT, [string]$orgName) {
     $username = ""
@@ -69,10 +143,7 @@ function Add-ADOUsertoGroup ([string]$userid, [string]$groupID,[string]$PAT, [st
    }
 }
 
-$identityMap = Import-Csv -Path ".\identitymap.csv"
-$ADSGroupInfo = Get-Content -Path ".\Export_ADSGroupMemberships.json" | ConvertFrom-Json
-$ADOGroupInfo = Get-ADOGroupInfo -PAT $ADOpat -orgName $ADOorgName
-$ADOUserInfo = Get-ADOUserInfo -PAT $ADOpat -orgName $ADOorgName
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
 
 foreach ($ADSgroup in $ADSGroupInfo) {
     $groupFound = $false
